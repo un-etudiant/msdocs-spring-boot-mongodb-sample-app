96235f28ee9ddc14e4667ce4ae026fc9f2977d09
Pradeep Sundaram <psundaram@newrelic.com>
2025-02-28 22:09:00 +0530

    Made changes to  logging

--- src/main/java/com/microsoft/azure/appservice/examples/springbootmongodb/controller/TodoListController.java
+++ src/main/java/com/microsoft/azure/appservice/examples/springbootmongodb/controller/TodoListController.java
@@ -3,8 +3,9 @@
 
 package com.microsoft.azure.appservice.examples.springbootmongodb.controller;
 
-import com.microsoft.azure.appservice.examples.springbootmongodb.dao.TodoItemRepository;
-import com.microsoft.azure.appservice.examples.springbootmongodb.model.TodoItem;
+import java.util.List;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-import java.util.UUID;
+import com.microsoft.azure.appservice.examples.springbootmongodb.dao.TodoItemRepository;
+import com.microsoft.azure.appservice.examples.springbootmongodb.model.TodoItem;
 
 @RestController
 public class TodoListController {
@@ -50,7 +51,8 @@ public class TodoListController {
         logger.info("GET request access '/api/todolist' path.");
 
         List<TodoItem> items = todoItemRepository.findAll();
-        return todoItemRepository.findAll();
+        logger.info("rertieved {} items",items.size());
+        return items;
     }
 
     /**
